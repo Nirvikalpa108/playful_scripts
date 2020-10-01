863e714bd23cb341072550e79e33ab9a402e225b
Amina Adewusi <amina.adewusi@guardian.co.uk>
2020-10-01 07:49:12 +0100

    polishing this up, removing comment lines etc

--- childdevelopment/src/main/scala/example/Hello.scala
+++ childdevelopment/src/main/scala/example/Hello.scala
@@ -4,7 +4,7 @@ import anorm._
 import scala.util.control.NonFatal
 
 // to run programme:
-//$ /Users/amina_adewusi/code/adam/childdevelopment/
+//$ cd /Users/amina_adewusi/code/adam/childdevelopment/
 //$ Sbt
 // "run $age"
 
@@ -13,16 +13,14 @@ object Hello {
   implicit val connection = DriverManager.getConnection("jdbc:sqlite:/Users/amina_adewusi/code/adam/childDevelopment.db")
   val parser = Macro.namedParser[Activity]
 
-  // all reading/writing from terminal to live in main eg all printlns
   def main(args: Array[String]): Unit = {
     try {
-      val ageInput = args.head //error
-      // val areaInput = args(1) //error
-      val validatedAgeInput = validateAgeInput(ageInput) //error
-      // val validatedAreaInput = ??? //do we need this?
+      val ageInput = args.head
+      val areaInput = args(1)
+      val validatedAgeInput = validateAgeInput(ageInput)
       val ageForDbQuery = extractAgeFromInput(validatedAgeInput)
-      // val areaForDbQuery = extractAreaFromInput(areaInput)
-      val dbResults = queryAgeDb(ageForDbQuery, None) //error
+      val areaForDbQuery = extractAreaFromInput(areaInput)
+      val dbResults = queryAgeDb(ageForDbQuery, areaForDbQuery)
       val formattedResults = formatActivities(dbResults)
       println(formattedResults)
     } catch {
@@ -35,28 +33,35 @@ object Hello {
 
   def extractAgeFromInput(ageInput: Int): Int = {
     ageInput match {
-      case n if n < 0 => throw new RuntimeException("The minimum age parameter is 0 - please try again!")
+      case n if n < 0 => throw new RuntimeException(
+        "The minimum age parameter is 0 - please try again!")
       case n if n < 8 => 0
       case n if n < 16 => 8
       case n if n < 22 => 16
       case n if n < 30 => 22
       case n if n < 40 => 30
       case n if n < 60 => 40
-      case _ => throw new RuntimeException("The maximum age parameter accepted is 59")
+      case _ => throw new RuntimeException(
+        "The maximum age parameter accepted is 59")
     }
   }
   
-  // decide if I want to tackle this fuzzy match problem or not
   def extractAreaFromInput(area: String): String = {
     area match {
       case "PSE" => "PSE"
+      case "PE" => "Physical Development"
+      case "Feelings" => "Managing feelings and behaviour"
       case "Communication" => "Communication and Language"
+      case "Literacy" => "Literacy"
+      case "Maths" => "Mathematics"
+      case "World" => "Understanding the World"
       case "Arts" => "Expressive Arts and Design"
     }
   }
 
-  def queryAgeDb(age: Int, area: Option[String]): List[Activity] = {
-    SQL"SELECT * FROM earlyYearsFoundationStageFramework WHERE age = ${age}".as(parser.*)
+  def queryAgeDb(age: Int, area: String): List[Activity] = {
+    SQL"SELECT * FROM earlyYearsFoundationStageFramework WHERE age = ${age} AND area = ${area}"
+      .as(parser.*)
     }
 
   def formatActivities(results: List[Activity]): String = {
